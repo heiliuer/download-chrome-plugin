ec2eb59d6e72628e90113c6867953d8a1ba9fa1c
wanghao51 <heiliuer@qq.com>
2017-12-29 15:23:32 +0800

    fixed

---
+++ src/page/PageHelp.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <fieldset style="padding: 20px;">
+            <legend>如何下载安装插件</legend>
+            <span style="font-size: 12px;color:#999;">
+                注：立即下载 | 点击右键 &gt; 链接另存为 &gt; 拖入下载的文件到此页面中
+                <a href="chrome://extensions/" target="chrome://extensions/"
+                   style="color:#4d90fe;">chrome://extensions/</a>
+            </span>
+        </fieldset>
+
+        <fieldset style="padding: 20px;">
+            <legend>搜索没反应</legend>
+            <span style="font-size: 12px;color:#999;vertical-align: top;">
+                点击地址栏 小按钮 <img src="../img/1514531631(1).jpg" style="margin-left: 10px;" alt="">
+            </span>
+        </fieldset>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "page-help"
+    }
+</script>
+
+<style scoped>
+
+</style>
