bf49ee5b04232e9ba5b38750582e850b090e71f1
wanghao51 <heiliuer@qq.com>
2017-12-29 18:05:40 +0800

    fixed

--- src/page/PageIndex.vue
+++ src/page/PageIndex.vue
@@ -1,8 +1,11 @@
 <template>
     <div id="searchPage" class="searchPage page" style="display:block;">
         <div style="padding: 20px;">
-            <input type="text" v-model="key" placeholder="输入插件名或者其他关键字" style="padding: 5px;width: 200px;"> <a href="#/help" style="margin-left: 10px;color:#555;">使用帮助</a>
+            <input type="text" v-model="key" placeholder="输入插件名或者其他关键字" style="padding: 5px;width: 200px;"> <a
+                href="#/help" style="margin-left: 10px;color:#555;">使用帮助</a>
         </div>
+        <p style="color: #555;text-align: center;width: 100%;" v-show="loading">正在加载...</p>
+        <p v-show="!loading&&items&&items.length===0&&!!key" style="color: #500;text-align: center;width: 100%;">没有找到相关插件</p>
         <ul id="searchList" style="display: block;">
             <li class="app-tag search-row" v-for="item in items">
                 <div class="search-info">
@@ -21,13 +24,17 @@
 </template>
 
 <script>
+    function log() {
+        // console.log(...arguments)
+    }
+
     export default {
         name: "page-index",
         data() {
             return {
                 key: '',
                 items: null,
-                loading: true
+                loading: false
             }
         },
         methods: {
@@ -49,18 +56,31 @@
             fetchData() {
                 const vm = this
                 const {key} = this
-                vm.items = null
                 vm.loading = true
+                if (this._fetchDataFlag) {
+                    this._fetchDataFlag.cancel = true
+                    log('cancel begin')
+                }
+                const fetchDataFlag = this._fetchDataFlag = {
+                    cancel: false
+                }
                 fetch(
                     `http://extension.browser.qq.com/search_v3?key=${key}`,
                     {method: 'get'}
                 ).then(res => res.json()).then(json => {
-                    console.log(json)
+                    if (!!fetchDataFlag.cancel) {
+                        log('cancel finished')
+                        return
+                    }
+                    log(json)
                     const itemList = json['result_list']['value'].map(item => JSON.parse(item['doc_meta']))
-                    console.log(itemList)
+                    log(itemList)
                     vm.items = itemList
                     vm.loading = null
                 }).catch(res => {
+                    if (!!fetchDataFlag.cancel) {
+                        return
+                    }
                     vm.loading = false
                     console.error(res)
                 })
@@ -75,6 +95,7 @@
         },
         watch: {
             key() {
+                this.items = null
                 this.fetchDataD()
             }
         }
